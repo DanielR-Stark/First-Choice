e26e750f3a7bab519a141342401ed02d9ada0276
daniel rivera daniel.rivera0823@gmail.com <daniel.rivera0823@gmail.com>
2022-10-15 13:01:06 -0500

    Controlador e index de education centers

---
+++ app/controllers/education_centers_controller.rb
@@ -0,0 +1,50 @@
+class EducationCentersController < ApplicationController
+  def index
+    @educationcenters = EducationCenter.all
+  end
+
+  def show
+    @educationcenter = EducationCenter.find(params[:id])
+  end
+
+  def new
+    @educationcenter = EducationCenter.new
+  end
+
+  def edit
+  end
+
+  def create
+    @educationcenter = EducationCenter.new(education_center_params)
+    @educationcenter.user = current_user
+    if @educationcenter.save
+      redirect_to education_center_path(@educationcenter)
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @educationcenter.update(education_center_params)
+      redirect_to education_center_path(@educationcenter)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @educationcenter = EducationCenter.find(params[:id])
+    @educationcenter.destroy
+    redirect_to education_center_path
+  end
+
+  private
+
+  def set_education_center
+    @educationcenter = EducationCenter.find(params[:id])
+  end
+
+  def education_center_params
+    params.require(:educationcenter).permit(:name, :city, :description)
+  end
+end
