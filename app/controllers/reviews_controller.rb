cae05fd9158af349cb1677bb4f79453a2e6663f9
Santiago Meneses <scordoba0817@gmail.com>
2022-10-15 15:02:09 -0500

    created private method and finishing up reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,15 @@
 class ReviewsController < ApplicationController
+  before_action :set_education_center
+  def new
+    @review = Review.new
+  end
+
+  def create
+  end
+
+  private
+
+  def set_education_center
+    @education_center = EducationCenter.find(params[:education_center_id])
+  end
 end
--- app/models/education_center.rb
+++ app/models/education_center.rb
@@ -1,3 +1,4 @@
 class EducationCenter < ApplicationRecord
   has_many :education_center_careers
+  has_many :reviews
 end
