8b771ddb8fd60bba95e0c91d7aab63573a6a5264
Santiago Meneses <scordoba0817@gmail.com>
2022-10-15 14:24:37 -0500

    added paths to review

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   resources :education_centers
+  resources :reviews
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
