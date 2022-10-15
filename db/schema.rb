960b03470a2df5d4974e613ed67ab69d956998d9
daniel rivera daniel.rivera0823@gmail.com <daniel.rivera0823@gmail.com>
2022-10-15 10:50:37 -0500

    Model enrollments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_15_154353) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_15_155005) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,12 +28,30 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_15_154353) do
     t.index ["career_category_id"], name: "index_careers_on_career_category_id"
   end
 
+  create_table "education_center_careers", force: :cascade do |t|
+    t.bigint "education_center_id", null: false
+    t.bigint "career_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["career_id"], name: "index_education_center_careers_on_career_id"
+    t.index ["education_center_id"], name: "index_education_center_careers_on_education_center_id"
+  end
+
   create_table "education_centers", force: :cascade do |t|
     t.string "name"
     t.string "city"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "enrollments", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "education_center_career_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["education_center_career_id"], name: "index_enrollments_on_education_center_career_id"
+    t.index ["user_id"], name: "index_enrollments_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -52,4 +70,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_15_154353) do
   end
 
   add_foreign_key "careers", "career_categories"
+  add_foreign_key "education_center_careers", "careers"
+  add_foreign_key "education_center_careers", "education_centers"
+  add_foreign_key "enrollments", "education_center_careers"
+  add_foreign_key "enrollments", "users"
 end
