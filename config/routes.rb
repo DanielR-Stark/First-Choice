d0b45cc72399194814b146d49d7f1188c3df4526
Santiago Meneses <scordoba0817@gmail.com>
2022-10-18 18:28:42 -0500

    added reviews as a nested resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :education_centers
-  resources :reviews
+  resources :education_centers do
+    resources :reviews
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
