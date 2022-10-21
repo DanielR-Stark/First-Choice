d0224398dcacae52cd97920ed3d7a511599fb77c
Santiago Meneses <scordoba0817@gmail.com>
2022-10-20 20:13:59 -0500

    adding changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,4 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-<<<<<<< HEAD
-  has_many :enrrollments
-  has_many :reviews
-=======
->>>>>>> 86fd446acac29481ec34ce3ce37a0a94969276e4
 end
