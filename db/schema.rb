8ca877fd2e13bb60ada310654f9f443cb834bf3c
Santiago Meneses <scordoba0817@gmail.com>
2022-10-20 23:26:12 -0500

    fixed issues with career model and created the table in the DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_15_185856) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_21_042521) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "careers", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.text "duration"
+    t.text "modality"
+    t.integer "education_center_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "education_centers", force: :cascade do |t|
     t.string "name"
