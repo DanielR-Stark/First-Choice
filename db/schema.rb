5fdf101edd1b697e3b6d40d8fd1837180dd217f0
santiago graciano santiagoga@algartech.com <santiagoga@algartech.com>
2022-10-22 16:38:30 -0500

    coronamos

--- db/schema.rb
+++ db/schema.rb
@@ -7,12 +7,23 @@
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
-#
+
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_22_155617) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_22_213301) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "careers", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.text "duration"
+    t.text "modality"
+    t.bigint "education_center_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["education_center_id"], name: "index_careers_on_education_center_id"
+  end
 
   create_table "education_centers", force: :cascade do |t|
     t.string "name"
@@ -48,6 +59,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_22_155617) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "careers", "education_centers"
   add_foreign_key "reviews", "education_centers"
   add_foreign_key "reviews", "users"
 end
