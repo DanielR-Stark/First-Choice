12f824a67981f0e94b390bfac27bc997a59e3504
Santiago Meneses <scordoba0817@gmail.com>
2022-11-05 12:12:08 -0500

    added the question policiy file

--- app/policies/resultado_policy.rb
+++ app/policies/resultado_policy.rb
@@ -1,8 +1,12 @@
 class ResultadoPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
-    # def resolve
-    #   scope.all
-    # end
+    def resolve
+      scope.all
+    end
+  end
+
+  def show?
+    return true
   end
 end
